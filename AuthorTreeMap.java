fc66adae4796bad706244887040ea4097163cb67
devanshaggarwal026 <50549721+devanshaggarwal026@users.noreply.github.com>
2020-01-23 17:59:45 +0530

    Update AuthorTreeMap.java

--- AuthorTreeMap.java
+++ AuthorTreeMap.java
@@ -7,16 +7,17 @@ public class AuthorTreeMap {
 		
 		TreeMap<String, Author> author = new TreeMap<String, Author>();
 		
+		HashSet<String> set = new HashSet<String>();
 		author.put("c", new Author(2 , "devansh"  , "a"));
-		//author.put("java", new Author(4 , "divya"  , "x"));
-		//author.put("python", new Author(7 , "roshan"  , "p"));
-		//author.put("c++", new Author(3 , "astha"  , "r"));
+		author.put("java", new Author(4 , "divya"  , "x"));
+		author.put("python", new Author(7 , "roshan"  , "p"));
+		author.put("c++", new Author(3 , "astha"  , "r"));
 		author.put("swift", new Author(5 , "tushar"  , "m"));
 		author.put("ruby", new Author(2 , "devansh"  , "a"));
 		author.put("scala", new Author(2 , "devansh"  , "a"));
 		author.put("hive", new Author(7 , "roshan"  , "p"));
 		author.put("docker", new Author(7 , "roshan"  , "p"));
-		//author.put("pig", new Author(3 , "astha"  , "r"));
+		author.put("pig", new Author(3 , "astha"  , "r"));
 		
 		
 		Set<String> keys = author.keySet();
@@ -26,15 +27,18 @@ public class AuthorTreeMap {
 			Author a = author.get(s);
 			
 			System.out.println(s + "  written by " + a.getName() + " residing at " + a.getAddress());
-			//String keey = s;
-			Author ar = a;
-			author.remove(a);
 			
-			boolean b = author.containsValue(a.getName());
-			System.out.println(b);
 		}
 		
-		//author.containsValue(value);
+		for(String s:keys) {
+			Author a = author.get(s);
+			
+			if (set.add(a.getName()) == false){
+				System.out.println("id is : " + a.getId() + "  name is :  " +a.getName()
+				+ " Address  is : "+ a.getAddress() );
+			} 
+			
+		}
 		
 	}
 
